bad03d1020500a4fd8fc1a1be143100cc298d2f3
aly.elba <aly.elba@kallysoft.ch>
2020-04-27 17:02:14 +0200

    fix server to accept form data multi part

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 7777;
 const VideoRequestData = require('./data/video-requests.data');
 const cors = require('cors');
 const mongoose = require('./models/mongo.config');
+const multer = require('multer');
 
 if (!Object.keys(mongoose).length) return;
 
@@ -16,7 +17,8 @@ app.get('/', (req, res) =>
   res.send('Welcome to semicolon academy APIs, use /video-request to get data')
 );
 
-app.post('/video-request', async (req, res, next) => {
+const upload = multer();
+app.post('/video-request', upload.none(), async (req, res, next) => {
   const response = await VideoRequestData.createRequest(req.body);
   res.send(response);
   next();
@@ -40,7 +42,7 @@ app.get('/users', async (req, res, next) => {
   next();
 });
 
-app.use(express.json());
+// app.use(express.json());
 app.post('/users/login', async (req, res, next) => {
   const response = await UserData.createUser(req.body);
   res.redirect(`http://localhost:5500?id=${response._id}`);
